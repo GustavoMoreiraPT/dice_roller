db6809885943fa81fdaa0cc3d244f4e8ee2b7186
Gustavo Moreira <gustavo1995.2012@hotmail.com>
2020-11-30 19:58:04 +0000

    Added cde optimizations

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,6 +7,9 @@ import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,12 +17,12 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
-    private fun rollDice() {
+        private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-
-        val diceImage: ImageView = findViewById(R.id.dice_image)
 
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
